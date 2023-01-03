20891841dc3baf5b4e896c9c4a7be6cc9f902e50
Yelkira <email@email.com>
2023-01-03 13:39:47 +0300

    update ts

---
+++ src/Todolist.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+type TodolistPropsType = {
+    shapka: string
+    //tasks: Array<TaskType>
+    tasks: TaskType[]
+}
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export const TodoList = (props: TodolistPropsType) => {
+    return (
+        <div>
+            <h3>{props.shapka}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map((el) => {
+                    return (
+                        <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
+                    )
+                })}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
